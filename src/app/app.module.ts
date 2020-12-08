3bc3648afe95095e15c23cade7fd0d353184808d
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2020-12-08 11:22:41 +0200

    fa input box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AuFaInputComponent
   ],
   imports: [
     BrowserModule,
